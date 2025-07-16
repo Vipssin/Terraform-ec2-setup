9d5a4d51bb6e1ba13cdcc7e5ecdd37a531dcb55f
Vipssin <122277548+Vipssin@users.noreply.github.com>
2025-07-16 10:36:21 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+  key_name      = var.terraformhachi
   tags = {
     Name = "Terraformproject"
   }
